fa956aace837cf89f11f8d1933150d28b4bb016f
Worathon3054 <52226326+Worathon3054@users.noreply.github.com>
2019-06-26 09:45:40 +0700

    Add files via upload

---
+++ main/WiFi.ino
@@ -0,0 +1,18 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "IT@WIFI";
+const char* password = "itnrodegat";
+
+void WiFiConnections() {
+   Serial.println("Starting...");
+
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) {
+      delay(250);
+      Serial.print(".");
+   }
+   Serial.println("\nWiFi connected");  
+   Serial.print("IP address: ");
+   Serial.print(WiFi.localIP());
+   
+}
